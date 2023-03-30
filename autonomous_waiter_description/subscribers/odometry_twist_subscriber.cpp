3bbc762264f395d41e02b363172cd8ba06d6b40f
MarteagaJ <mjarteaga2019@gmail.com>
2023-03-30 06:27:15 -0400

    odometry_twist_subscriber subscriber setup

---
+++ autonomous_waiter_description/subscribers/odometry_twist_subscriber.cpp
@@ -0,0 +1,31 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/Twist.hpp"
+using std::placeholders::_1;
+
+class OdometryTwistSubscriber : public rclcpp::Node
+{
+  public:
+    OdometryTwistSubscriber()
+    : Node("odometry_twist_subscriber")
+    {
+      subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
+      "odometry", 10, std::bind(&OdometryTwistSubscriber::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const geometry_msgs::msg::Twist & msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: LinearX => %d, AngularZ => %d", msg.linear.x, msg.angular.z);
+    }
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<OdometryTwistSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
