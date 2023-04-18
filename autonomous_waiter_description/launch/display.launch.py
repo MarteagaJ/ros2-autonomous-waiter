c1fd6fbdcf2bbae4eeae6a82e7d5ceec6f43e117
MarteagaJ <mjarteaga2019@gmail.com>
2023-04-18 02:45:51 -0400

    path planning launch files

--- autonomous_waiter_description/launch/display.launch.py
+++ autonomous_waiter_description/launch/display.launch.py
@@ -15,10 +15,12 @@ def generate_launch_description():
     # default_ekf_params_file = os.path.join(pkg_share, 'config/ekf.yaml')
 
     lifecycle_nodes = ['controller_server',
-                       'planner_server',
-                       'recoveries_server',
-                       'bt_navigator',
-                       'waypoint_follower']
+                    'smoother_server',
+                    'planner_server',
+                    'behavior_server',
+                    'bt_navigator',
+                    'waypoint_follower',
+                    'velocity_smoother']
     
     # Map fully qualified names to relative ones so the node's namespace can be prepended.
     # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
@@ -28,20 +30,26 @@ def generate_launch_description():
     #              https://github.com/ros2/launch_ros/issues/56
     remappings = [('/tf', 'tf'),
                   ('/tf_static', 'tf_static'),
-                  ('/cmd_vel', '/diff_cont/cmd_vel_unstamped')]
+                  ('cmd_vel_nav', '/diff_cont/cmd_vel_unstamped')]
     
     # Create our own temporary YAML files that include substitutions
     param_substitutions = {
         'use_sim_time': LaunchConfiguration('use_sim_time'),
-        'default_bt_xml_filename': LaunchConfiguration('default_bt_xml_filename'),
-        'autostart': LaunchConfiguration('autostart'),
-        'map_subscribe_transient_local': LaunchConfiguration('map_subscribe_transient_local')}
+        # 'default_bt_xml_filename': LaunchConfiguration('default_bt_xml_filename'),
+        'autostart': LaunchConfiguration('autostart')
+        # 'map_subscribe_transient_local': LaunchConfiguration('map_subscribe_transient_local')
+    }
 
     nav2_configured_params = RewrittenYaml(
-            source_file=LaunchConfiguration('nav2_params_file'),
-            root_key=LaunchConfiguration('namespace'),
-            param_rewrites=param_substitutions,
-            convert_types=True)
+        source_file=LaunchConfiguration('nav2_params_file'),
+        root_key=LaunchConfiguration('namespace'),
+        param_rewrites=param_substitutions,
+        convert_types=True
+    )
+
+
+
+
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
@@ -160,7 +168,22 @@ def generate_launch_description():
         package='nav2_controller',
         executable='controller_server',
         output='screen',
+        respawn=LaunchConfiguration('use_respawn'),
+        respawn_delay=2.0,
         parameters=[nav2_configured_params],
+        arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')],
+        remappings=remappings + [('cmd_vel', 'cmd_vel_nav')]
+    )
+
+    nav2_smoother_server = launch_ros.actions.Node(
+        package='nav2_smoother',
+        executable='smoother_server',
+        name='smoother_server',
+        output='screen',
+        respawn=LaunchConfiguration('use_respawn'),
+        respawn_delay=2.0,
+        parameters=[nav2_configured_params],
+        arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')],
         remappings=remappings
     )
 
@@ -169,16 +192,22 @@ def generate_launch_description():
         executable='planner_server',
         name='planner_server',
         output='screen',
+        respawn=LaunchConfiguration('use_respawn'),
+        respawn_delay=2.0,
         parameters=[nav2_configured_params],
+        arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')],
         remappings=remappings
     )
 
-    nav2_recoveries_server = launch_ros.actions.Node(
-        package='nav2_recoveries',
-        executable='recoveries_server',
-        name='recoveries_server',
+    nav2_behavior_server = launch_ros.actions.Node(
+        package='nav2_behaviors',
+        executable='behavior_server',
+        name='behavior_server',
         output='screen',
+        respawn=LaunchConfiguration('use_respawn'),
+        respawn_delay=2.0,
         parameters=[nav2_configured_params],
+        arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')],
         remappings=remappings
     )
 
@@ -187,7 +216,10 @@ def generate_launch_description():
         executable='bt_navigator',
         name='bt_navigator',
         output='screen',
+        respawn=LaunchConfiguration('use_respawn'),
+        respawn_delay=2.0,
         parameters=[nav2_configured_params],
+        arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')],
         remappings=remappings
     )
 
@@ -196,8 +228,24 @@ def generate_launch_description():
         executable='waypoint_follower',
         name='waypoint_follower',
         output='screen',
+        respawn=LaunchConfiguration('use_respawn'),
+        respawn_delay=2.0,
         parameters=[nav2_configured_params],
+        arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')],
         remappings=remappings
+    )
+
+    nav2_velocity_smoother = launch_ros.actions.Node(
+        package='nav2_velocity_smoother',
+        executable='velocity_smoother',
+        name='velocity_smoother',
+        output='screen',
+        respawn=LaunchConfiguration('use_respawn'),
+        respawn_delay=2.0,
+        parameters=[nav2_configured_params],
+        arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')],
+        remappings=remappings +
+                [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')]
     )
 
     nav2_lifecycle_manager = launch_ros.actions.Node(
@@ -205,9 +253,24 @@ def generate_launch_description():
         executable='lifecycle_manager',
         name='lifecycle_manager_navigation',
         output='screen',
+        arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')],
         parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')},
                     {'autostart': LaunchConfiguration('autostart')},
                     {'node_names': lifecycle_nodes}]
+    )
+
+    delayed_nav2_nodes = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=slam_node,
+            on_start=[nav2_controller_server,
+                nav2_smoother_server,
+                nav2_planner_server,
+                nav2_behavior_server,
+                nav2_bt_navigator,
+                nav2_waypoint_follower,
+                nav2_velocity_smoother,
+                nav2_lifecycle_manager]
+        )
     )
 
     # diff_drive_spawner = launch_ros.actions.Node(
@@ -243,6 +306,14 @@ def generate_launch_description():
                                             description='Whether to set the map subscriber QoS to transient local'),
         launch.actions.DeclareLaunchArgument('namespace', default_value='',
                                             description='Top-level namespace'),
+        launch.actions.DeclareLaunchArgument('use_composition', default_value='False',
+                                            description='Use composed bringup if True'),
+        launch.actions.DeclareLaunchArgument('container_name', default_value='nav2_container',
+                                            description='the name of conatiner that nodes will load in if use composition'),
+        launch.actions.DeclareLaunchArgument('use_respawn', default_value='False',
+                                            description='Whether to respawn if a node crashes. Applied when composition is disabled.'),
+        launch.actions.DeclareLaunchArgument('log_level', default_value='info',
+                                            description='log level'),
         # launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
         #                                     description='Absolute path to rviz config file'),
         robot_state_publisher_node,
@@ -255,5 +326,14 @@ def generate_launch_description():
         rplidar_node,
         # slam_node
         delayed_slam_node,
+        # nav2_controller_server,
+        # nav2_smoother_server,
+        # nav2_planner_server,
+        # nav2_behavior_server,
+        # nav2_bt_navigator,
+        # nav2_waypoint_follower,
+        # nav2_velocity_smoother,
+        # nav2_lifecycle_manager,
+        delayed_nav2_nodes,
         rviz_node
     ])
