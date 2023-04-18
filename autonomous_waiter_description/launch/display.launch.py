4a36503446911e53b3252d518a820c42941b9835
MarteagaJ <mjarteaga2019@gmail.com>
2023-04-18 01:31:15 -0400

    nav 2 stuff

--- autonomous_waiter_description/launch/display.launch.py
+++ autonomous_waiter_description/launch/display.launch.py
@@ -1,17 +1,47 @@
 import launch
 from launch.substitutions import Command, LaunchConfiguration
-from launch.actions import TimerAction, RegisterEventHandler
+from launch.actions import TimerAction, RegisterEventHandler, SetEnvironmentVariable
 from launch.event_handlers import OnProcessStart
 import launch_ros
 import os
 from ament_index_python.packages import get_package_share_directory
+from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='autonomous_waiter_description').find('autonomous_waiter_description')
     default_model_path = os.path.join(pkg_share, 'description/autonomous_waiter_description.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'config/urdf_config.rviz')
     default_slam_params_file = os.path.join(pkg_share, 'config/mapper_params_online_async.yaml')
-    default_ekf_params_file = os.path.join(pkg_share, 'config/ekf.yaml')
+    # default_ekf_params_file = os.path.join(pkg_share, 'config/ekf.yaml')
+
+    lifecycle_nodes = ['controller_server',
+                       'planner_server',
+                       'recoveries_server',
+                       'bt_navigator',
+                       'waypoint_follower']
+    
+    # Map fully qualified names to relative ones so the node's namespace can be prepended.
+    # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
+    # https://github.com/ros/geometry2/issues/32
+    # https://github.com/ros/robot_state_publisher/pull/30
+    # TODO(orduno) Substitute with `PushNodeRemapping`
+    #              https://github.com/ros2/launch_ros/issues/56
+    remappings = [('/tf', 'tf'),
+                  ('/tf_static', 'tf_static'),
+                  ('/cmd_vel', '/diff_cont/cmd_vel_unstamped')]
+    
+    # Create our own temporary YAML files that include substitutions
+    param_substitutions = {
+        'use_sim_time': LaunchConfiguration('use_sim_time'),
+        'default_bt_xml_filename': LaunchConfiguration('default_bt_xml_filename'),
+        'autostart': LaunchConfiguration('autostart'),
+        'map_subscribe_transient_local': LaunchConfiguration('map_subscribe_transient_local')}
+
+    nav2_configured_params = RewrittenYaml(
+            source_file=LaunchConfiguration('nav2_params_file'),
+            root_key=LaunchConfiguration('namespace'),
+            param_rewrites=param_substitutions,
+            convert_types=True)
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
@@ -126,6 +156,60 @@ def generate_launch_description():
         )
     )
 
+    nav2_controller_server = launch_ros.actions.Node(
+        package='nav2_controller',
+        executable='controller_server',
+        output='screen',
+        parameters=[nav2_configured_params],
+        remappings=remappings
+    )
+
+    nav2_planner_server = launch_ros.actions.Node(
+        package='nav2_planner',
+        executable='planner_server',
+        name='planner_server',
+        output='screen',
+        parameters=[nav2_configured_params],
+        remappings=remappings
+    )
+
+    nav2_recoveries_server = launch_ros.actions.Node(
+        package='nav2_recoveries',
+        executable='recoveries_server',
+        name='recoveries_server',
+        output='screen',
+        parameters=[nav2_configured_params],
+        remappings=remappings
+    )
+
+    nav2_bt_navigator = launch_ros.actions.Node(
+        package='nav2_bt_navigator',
+        executable='bt_navigator',
+        name='bt_navigator',
+        output='screen',
+        parameters=[nav2_configured_params],
+        remappings=remappings
+    )
+
+    nav2_waypoint_follower = launch_ros.actions.Node(
+        package='nav2_waypoint_follower',
+        executable='waypoint_follower',
+        name='waypoint_follower',
+        output='screen',
+        parameters=[nav2_configured_params],
+        remappings=remappings
+    )
+
+    nav2_lifecycle_manager = launch_ros.actions.Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_navigation',
+        output='screen',
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')},
+                    {'autostart': LaunchConfiguration('autostart')},
+                    {'node_names': lifecycle_nodes}]
+    )
+
     # diff_drive_spawner = launch_ros.actions.Node(
     #     package="controller_manager",
     #     executable="spawner.py",
@@ -138,12 +222,27 @@ def generate_launch_description():
     # )
 
     return launch.LaunchDescription([
+        # Set env var to print messages to stdout immediately
+        SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
         launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
                                             description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='False',
                                             description='Flag to enable use_sim_time'),
+        launch.actions.DeclareLaunchArgument('autostart', default_value='true',
+                                            description='Automatically startup the nav2 stack'),
+        launch.actions.DeclareLaunchArgument('default_bt_xml_filename',
+                                            default_value=os.path.join(
+                                                get_package_share_directory('nav2_bt_navigator'),
+                                                'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
+                                            description='Full path to the behavior tree xml file to use'),
+        launch.actions.DeclareLaunchArgument('nav2_params_file', default_value=os.path.join(pkg_share, 'config', 'nav2_params.yaml'),
+                                            description='Full path to the ROS2 parameters file to use'),
+        launch.actions.DeclareLaunchArgument('map_subscribe_transient_local', default_value='false',
+                                            description='Whether to set the map subscriber QoS to transient local'),
+        launch.actions.DeclareLaunchArgument('namespace', default_value='',
+                                            description='Top-level namespace'),
         # launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
         #                                     description='Absolute path to rviz config file'),
         robot_state_publisher_node,
