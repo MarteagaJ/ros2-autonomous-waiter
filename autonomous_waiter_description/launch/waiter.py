c224d1708dbc77a5fcfde2560aa4bd42892bced5
MarteagaJ <mjarteaga2019@gmail.com>
2023-04-20 03:59:17 -0400

    almost finished goal pose publishing, just still need to figure out what topic to send it to

--- autonomous_waiter_description/launch/waiter.py
+++ autonomous_waiter_description/launch/waiter.py
@@ -12,9 +12,20 @@ def set_velocity(x_velocity: float, z_velocity: float):
     # cmd_str = "ros2 param set /cmd_vel_publisher forward_vel {}".format(float(velocity))
     subprocess.run(cmd_str, shell=True)
 
+def set_goal_pose(x: float, y: float, z: float, theta: float):
+    # cmd_str = "ros2 topic pub --once /diff_cont/cmd_vel_unstamped geometry_msgs/msg/Twist {linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}
+    header_str = "header: " + "{" + "stamp: " + "{" + "sec: {}, nanosec: {}".format(int(0), int(0)) + "}" + ", frame_id: \"map\"" + "}"
+    pose_str = "pose: " + "{" + "position: " + "{" + "x: {}, y: {}, z: {}".format(x, y, z) + "}" + ", orientation: " + "{" + "x: 0.0, y: 0.0, z: {}, w: 1.0".format(theta) + "}" + "}"
+    cmd_str = "ros2 topic pub --once /diff_cont/cmd_vel_unstamped geometry_msgs/msg/PoseStamped \"" + "{" + "{}, {}".format(header_str, pose_str) + "}" + "\""
+    print(cmd_str)
+    # cmd_str = "ros2 param set /cmd_vel_publisher forward_vel {}".format(float(velocity))
+    # subprocess.run(cmd_str, shell=True)    
+
 def main(args=None):
     while(True):
-        set_velocity(float(0.3), float(0))
+        for i in range(5):
+            set_goal_pose(float(i), float(i), float(i), float(i))
+        # set_velocity(float(0.3), float(0))
 
 if __name__ == '__main__':
     main()
